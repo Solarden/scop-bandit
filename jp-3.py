d450a6756caea57e8f0437fa14f147e2b7f022b9
Solarden <pchraczynski@gmail.com>
2023-05-08 13:26:07 +0200

    jp3 init

---
+++ jp-3.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route("/users")
+def get_users():
+    name = request.args.get("name", None)
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    if not name:
+        c.execute("SELECT * FROM users WHERE name = ?", (name,))
+    else:
+        c.execute("SELECT * FROM users")
+    results = c.fetchall()
+    conn.close()
+    return str(results)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
